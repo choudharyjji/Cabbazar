b21ec0977c4a537bc6a59410832414d1cd08f76e
deepanshu <deepanshu@bigdrop.io>
2019-02-07 17:27:40 +0530

    new code

--- rn-cli.config.js
+++
@@ -1,7 +0,0 @@
-const blacklist = require('metro-config/src/defaults/blacklist');
-
-module.exports = {
-  resolver: {
-    blacklistRE: blacklist([/Users\/deepanshurustagi\/projects\/react_native\/cabbazaruser\/cabbazar_user\/node_modules\/react-native-razorpay\/node_modules\/.*/])
-  }
-};
